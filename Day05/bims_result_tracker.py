933fbab357a919db8d6bf502d186fb986a75ffe8
Aashan-Raja <aaashan502@gmail.com>
2026-06-08 15:34:00 +0500

    Completed BIMS Result Tracker

--- Day05/bims_result_tracker.py
+++ Day05/bims_result_tracker.py
@@ -1,9 +1,36 @@
 semester_result = {}
 while True:
-        semester_result["Name"] = input("Enter Subject Name (Or type 'Exit' to finish) :")
-        semester_result["Marks"] = int(input(f"Enter Marks of :  {semester_result["Name"]}  (Out of 100) :"))
-        for i in semester_result:
-            sheet =semester_result[i]
-        print(sheet)
+    try:
+        name = input("Enter Subject Name or (Type 'exit' to see Complete Mark Sheet)  :  ")
+        if name.lower() == "exit":
+            sub_count = 0
+            obtained_marks = 0
+            for subject , numbers in semester_result.items():
+                print(subject ,  numbers)
+                obtained_marks += numbers
+                sub_count = sub_count + 1
+            total_marks = sub_count * 100
+            percentage = (obtained_marks / total_marks) * 100
+            print(f"Total Obtained Marks are {obtained_marks}")
+            print(f"Percentage : {percentage}")
+            if percentage >= 85:
+                print("Grade : 'A' ")
+            elif percentage >= 76:
+                print("Grade : 'B' ")
+            elif percentage >= 65:
+                print("Grade : 'C' ")
+            elif percentage >= 50:
+                print("Grade : 'D' ")
+            else:
+                print("Grade : 'F' ")
+            break
+        else:
+            marks = int(input(f"Enter Obtained Marks of {name} Out of 100 :  "))
+            if marks >= 0 and marks <= 100:
+                semester_result[name] = marks
+            else:
+                print("Error! Marks Should Not be Greater then 100")
+    except Exception as err:
+        print(f"An error occured as {err}")
+            
         
-    
